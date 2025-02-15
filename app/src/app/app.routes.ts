e6111af5ae1be5df925aac6ecb4dfd6943a09bd0
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2025-02-15 17:40:00 -0300

    Configuracion de proyecto

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppngModule{}
